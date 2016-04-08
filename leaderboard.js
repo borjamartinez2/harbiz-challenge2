b2cd96c8d9c88b950aef8a669121d6410db696ef
alexta <terpyakov@gmail.com>
2016-04-08 10:27:40 +0300

    update Meteor release 1.3.1

--- leaderboard.js
+++ leaderboard.js
@@ -37,8 +37,7 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie",
-                   "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
+      var names = ["Ada Lovelace", "Grace Hopper", "Marie Curie", "Carl Friedrich Gauss", "Nikola Tesla", "Claude Shannon"];
       _.each(names, function (name) {
         Players.insert({
           name: name,
